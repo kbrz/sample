08f123223e6f833382733e7389fe12d8f741aa81
Konrad Brzykcy <konrad.brzykcy@rst-it.com>
2017-05-25 13:33:23 +0200

    views: added collapsing toolbar layouts with material header

---
+++ app/src/main/kotlin/net/kbrz/sample/util/BindingAdapters.kt
@@ -0,0 +1,24 @@
+package net.kbrz.sample.util
+
+import android.databinding.BindingAdapter
+import android.widget.ImageView
+import net.kbrz.sample.R
+
+/**
+ * @author Konrad Brzykcy
+ * @since 25.05.17
+ */
+
+@BindingAdapter("repoDetailsHeader")
+fun setRepoDetailsHeader(imageView: ImageView, repoName: String) {
+    val backgroundsCount = 4
+    val index = repoName.hashCode() % backgroundsCount
+    val headerDrawableId = when (index) {
+        0 -> R.drawable.details_bar_bg_1
+        1 -> R.drawable.details_bar_bg_2
+        2 -> R.drawable.details_bar_bg_3
+        3 -> R.drawable.details_bar_bg_4
+        else -> R.drawable.details_bar_bg_1
+    }
+    imageView.setImageResource(headerDrawableId)
+}
